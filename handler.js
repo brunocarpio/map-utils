f64a1e131720951ab8dd69ee5ebfae357f895898
Bruno Carpio <bcarpio_pe@pm.me>
2022-12-05 09:08:22 -0500

    feature with header row added

--- handler.js
+++ handler.js
@@ -1,11 +1,17 @@
 const fs = require("fs");
 
-let FF = fs.readFileSync("./ff-noheader.csv");
-FF = FF.toString();
+let FF = fs.readFileSync("./ff.csv", { encoding: "utf-8" });
 
-let obj = FF.split(/\n/)
+let splitted = FF.split(/\n/)
 	.filter((line) => line !== "")
-	.map((line) => line.split("|"))
-	.map((arr) => Object.assign({}, arr));
+	.map((line) => line.split("|"));
+// console.log(splitted);
+
+let headers = splitted.shift();
+// console.log(headers);
+
+let obj = splitted
+	.map((arr) => arr.map((elem, index) => [headers[index], elem]))
+	.map((arr) => Object.fromEntries(arr));
 
 console.log(obj);
